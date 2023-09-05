7fc5d54a4777095f2f9c1c1df66263cfba413e12
Farhad7860 <143691886+Farhad7860@users.noreply.github.com>
2023-09-05 21:15:08 +0530

    Add files via upload

---
+++ PrintInteger.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+import java .util.Scanner;
+public class PrintInteger
+{
+	public static void main(String[] args) {
+	    Scanner reader = new Scanner ( System.in);
+	    System.out.println("Required packages have been imported");
+	    System.out.print ("A reader object has been defined");
+	    System.out.print (" Enter an integer:");
+	    int number = reader.nextInt ();
+    System.out.print (" the next Int  method is used to read the integer value ");
+		System.out.println("The intrger is:" + number);
+	}
+}
