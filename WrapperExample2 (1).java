a98ea408ab25253dff32983d99d0b54f70fc3127
Farhad7860 <143691886+Farhad7860@users.noreply.github.com>
2023-09-06 11:57:28 +0530

    Add files via upload

---
+++ WrapperExample2 (1).java
@@ -0,0 +1,10 @@
+
+public class WrapperExample2
+{
+	public static void main(String[] args) {
+	    float a=1.65F;
+	    Float i=Float.valueOf(a);
+	    Float j=a;
+		System.out.println(a+""+i+""+j);
+	}
+}
